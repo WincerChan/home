f346a391ed0bfef303631c41a1672dcc61998551
WincerChan <wincerchan@gmail.com>
2018-07-19 12:24:33 +0800

    Add first page

---
+++ src/Name.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Name = () => (
+    <section className="hero is-fullheight">
+        <div className="container has-text-centered">
+            <h4 class="title is-5">hi, I'm <strong>Wincer</strong> /ˈwɪnsə(r)/ </h4>
+
+        </div>
+    </section >
+)
+
+export default Name;
