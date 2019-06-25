73f5e6428534ddc942e2547b5521fdfdf6ea55d6
WincerChan <WincerChan@gmail.com>
2019-06-25 09:30:49 +0800

    :wrench: Add <a> tag for project show

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,55 +1,48 @@
 import React from 'react';
-const footerStars = [
-    {
-        "name": "Meme",
-        "url": "https://github.com/WincerChan/Meme-generator",
-        "description": "A tool for generate meme.",
-        "image": "https://bulma.io/images/footer/bootstrap-to-bulma.png",
-        "refer": "View it on GitHub"
-    },
-    {
-        "name": "Hitokoto",
-        "url": "https://github.com/WincerChan/Hitokoto",
-        "description": "A self-build hitokoto API.",
-        "image": "https://bulma.io/images/footer/bootstrap-to-bulma.png",
-        "refer": "View it on GitHub"
-    },
-    {
-        "name": "QQMusic Parse",
-        "url": "https://github.com/WincerChan/QQMusic-Parse",
-        "description": "Pure JavaScript parse for QQ music.",
-        "image": "https://bulma.io/images/footer/bootstrap-to-bulma.png",
-        "refer": "View it on GitHub"
-    }
-]
 
 const BdFooterStars = () => (
     <div class="tile is-ancestor">
         <div class="tile is-parent is-3">
             <article class="tile is-child notification one showdelay">
-                <p class="subtitle">DIEM API</p>
-                <p class="dontShow">There are Two APIs: Hitokoto and Netease Cloud Music.</p>
+                <a href="https://api.itswincer.com" class="jump">
+                    <div className="jump">
+                        <p class="subtitle">DIEM API</p>
+                        <p class="dontShow">There are Two APIs: Hitokoto and Netease Cloud Music.</p>
+                    </div>
+                </a>
             </article>
         </div>
         <div class="tile is-vertical is-4">
             <div class="tile">
                 <div class="tile is-parent is-vertical">
                     <article class="tile is-child notification two showdelay">
-                        <p class="subtitle">Meme</p>
-                        <p class="dontShow">A tool(Written by JavaScript) for generate meme.</p>
+                        <a href="https://meme.itswincer.com" class="href">
+                            <div className="jump">
+                                <p class="subtitle">Meme</p>
+                                <p class="dontShow">A tool(Written by JavaScript) for generate meme.</p>
+                            </div>
+                        </a>
                     </article>
                 </div>
                 <div class="tile is-parent">
                     <article class="tile is-child notification three showdelay">
-                        <p class="subtitle">Douban Export</p>
-                        <p class="dontShow">Export all Douban items which marked.</p>
+                        <a href="https://github.com/WincerChan/douban-import" class="href">
+                            <div className="jump">
+                                <p class="subtitle">Douban Export</p>
+                                <p class="dontShow">Export all Douban items which marked.</p>
+                            </div>
+                        </a>
                     </article>
                 </div>
             </div>
             <div class="tile is-parent">
                 <article class="tile is-child notification four showdelay">
-                    <p class="subtitle">QQMusic Parse</p>
-                    <p class="dontShow">JavaScript parse for QQ music.</p>
+                    <a href="https://github.com/WincerChan/QQMusic-Parse" class="href">
+                        <div className="jump">
+                            <p class="subtitle">QQMusic Parse</p>
+                            <p class="dontShow">JavaScript parse for QQ music.</p>
+                        </div>
+                    </a>
                 </article>
             </div>
         </div>
